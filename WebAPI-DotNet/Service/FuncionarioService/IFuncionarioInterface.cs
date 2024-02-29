3032385f0efa0e700a6536cc05077935caf9af3b
amorimfe7 <amorimfe7@gmail.com>
2024-02-28 21:20:10 -0300

    criando Interface

--- WebAPI-DotNet/Service/FuncionarioService/IFuncionarioInterface.cs
+++ WebAPI-DotNet/Service/FuncionarioService/IFuncionarioInterface.cs
@@ -1,6 +1,13 @@
-﻿namespace WebAPI_DotNet.Service
+﻿namespace WebAPI_DotNet.Service.FuncionarioService
 {
-    public class FuncionarioInterface
+    public interface IFuncionarioInterface
     {
+        Task<ServiceResponse<List<FuncionarioModel>>> GetFuncionarios();
+        Task<ServiceResponse<List<FuncionarioModel>>> CreateFuncionario(FuncionarioModel novoFuncionario);
+        Task<ServiceResponse<FuncionarioModel>> GetFuncionarioById(int id);
+        Task<ServiceResponse<List<FuncionarioModel>>> UpdateFuncionario(FuncionarioModel editadoFuncionario);
+        Task<ServiceResponse<List<FuncionarioModel>>> DeleteFuncionario(int id);
+        Task<ServiceResponse<List<FuncionarioModel>>> InativaFuncionario(int id);
+        Task<ServiceResponse<List<FuncionarioModel>>> AtivaFuncionario(int id);
     }
 }
